8237efce311b641cc4a059fb383c93e1ff9b09e6
Igor <igor132143@gmail.com>
2023-09-11 21:00:13 +0300

    create hook `useFetching` and add loader

--- src/components/UI/Loader/Loader.jsx
+++ src/components/UI/Loader/Loader.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import cl from './Loader.module.css';
 
 const Loader = () => {
 
     return (
-        <div className={}>
+        <div className={cl.loader}>
 
         </div>
     )
 }
+
+export default Loader;
